a1d7c2c7c236f7d95fae91092e3410f27ea4e215
Ubuntu <ubuntu@ip-172-31-30-5.ec2.internal>
2021-11-21 14:42:46 +0000

    Changed Logs.swift

--- Logs.swift
+++ Logs.swift
@@ -1,29 +1,30 @@
 //
 //  Logs.swift
 //
-//  Created by Ryan Chung
+//  Created by Jenoe Balote
 //  Created on 2021-11-18
 //  Version 1.0
-//  Copyright (c) 2020 Ryan Chung. All rights reserved.
+//  Copyright (c) 2020 Jenoe Balote. All rights reserved.
 //
 //  This program calculates the amount of logs than can be put in a truck. 
 //
 // Variable Declaration
 let maxWeight: Float = 1100
-let logWeightPerMeter: Float = 20
-var heightOfLog: Float
+let density: Float = 20
+var logLength: Float
 var maxAmountOfLogs: Float
 
 // Input
 print("Enter the log length (0.25, 0.5 or 1 meters): ", terminator: "")
 
 // Converts input from string to float
-heightOfLog = Float(readLine()!)!
+logLength = Float(readLine()!)!
 
 // Calculates log capacity of the truck
-maxAmountOfLogs = maxWeight / logWeightPerMeter / heightOfLog
+maxAmountOfLogs = maxWeight / density / logLength
 
 // Output
-print("The truck can hold \(maxAmountOfLogs) logs.")
+print("The truck will be able to take \(maxAmountOfLogs) logs that are",
+      "\(logLength) meter(s) in length.")
 
 print("\nDone.")
